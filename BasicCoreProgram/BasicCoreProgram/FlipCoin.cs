f4fda97cb21b0cb43348c1ca550d11ca09317be6
pappurathod <prathod4545@gmail.com>
2022-03-14 14:07:54 +0530

    Flip Coin and print percentage of Heads and Tails

---
+++ BasicCoreProgram/BasicCoreProgram/FlipCoin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class FlipCoin
+    {
+        public double Headper = 0;
+        public double Tailper = 0;
+        public int Head = 0;
+        public int Tail = 0;
+
+        public void Display()
+        {
+            Console.WriteLine("Enter the how many number of time to flip coin");
+            int times = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < times; i++)
+            {
+                Random random = new Random();
+                int toss = random.Next(0, 2);
+                if (toss == 0)
+                {
+                    Head++;
+                }
+                if (toss == 1)
+                {
+                    Tail++;
+                }
+
+            }
+            Headper = ((double)Head / times * 100);
+            Console.WriteLine("Total Head = " + Head + ", Percentage of Heads = " + Headper);
+
+            Tailper = ((double)Tail / times * 100);
+            Console.WriteLine("Total Tail = " + Tail + ", Percentage of Tails = " + Tailper);
+        }
+
+    }
+}
---
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicCoreProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FlipCoin fc = new FlipCoin();
+            fc.Display();
+
+        }
+    }
+}
