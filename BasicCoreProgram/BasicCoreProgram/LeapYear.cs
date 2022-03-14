6acdfc1a60b68eebb3910d14ae7189be185db86f
pappurathod <prathod4545@gmail.com>
2022-03-14 14:21:26 +0530

    Check Leap Year or not Program

---
+++ BasicCoreProgram/BasicCoreProgram/LeapYear.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class LeapYear
+    {
+        public void Leap()
+        {
+            Console.WriteLine("Enter year");
+            int year = int.Parse(Console.ReadLine());
+            if (year > 999 && year < 10000)
+            {
+                if (DateTime.IsLeapYear(year))
+                {
+                    Console.WriteLine("{0} is Leap year", year);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not Leap year", year);
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter valid 4 digit year");
+            }
+        }
+
+    }
+}
